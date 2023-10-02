40e81fe770196c81594d3a1cca689a9cadfbd91b
TheRedPanda17 <calebandrewrobinson@gmail.com>
2023-10-02 08:57:29 -0500

    Add revoking

--- yoyo/migrations/story_20231001_01_nVple-add-permissions.sql
+++ yoyo/migrations/story_20231001_01_nVple-add-permissions.sql
@@ -15,11 +15,14 @@ CREATE TABLE "users_permissions" (
   "user_id" int,
   "created_by" int,
   "created_dt" timestamptz,
+  "revoked_by" int,
+  "revoked_dt" timestamptz,
   PRIMARY KEY(permission_id, user_id)
 );
 
 ALTER TABLE "users_permissions" ADD FOREIGN KEY ("permission_id") REFERENCES "permissions" ("id");
 ALTER TABLE "users_permissions" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+ALTER TABLE "users_permissions" ADD FOREIGN KEY ("revoked_by") REFERENCES "users" ("id");
 ALTER TABLE "users_permissions" ADD FOREIGN KEY ("created_by") REFERENCES "users" ("id");
 ALTER TABLE "permissions" ADD CONSTRAINT "permissions_unique_names" UNIQUE ("name");
 
@@ -29,6 +32,6 @@ INSERT INTO "permissions" VALUES(DEFAULT, 'view_users_permissions', NOW(), NOW()
 
 INSERT INTO "users" VALUES(DEFAULT, 'Admin', 'change_this', NOW(), NOW());
 
-INSERT INTO "users_permissions" VALUES(1, 1, 1, NOW());
-INSERT INTO "users_permissions" VALUES(2, 1, 1, NOW());
-INSERT INTO "users_permissions" VALUES(3, 1, 1, NOW());
+INSERT INTO "users_permissions" (permission_id, user_id, created_by, created_dt) VALUES(1, 1, 1, NOW());
+INSERT INTO "users_permissions" (permission_id, user_id, created_by, created_dt) VALUES(2, 1, 1, NOW());
+INSERT INTO "users_permissions" (permission_id, user_id, created_by, created_dt) VALUES(3, 1, 1, NOW());
