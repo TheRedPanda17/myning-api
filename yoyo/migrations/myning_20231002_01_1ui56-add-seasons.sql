3664738b664cf9440b149d592791dcac49af04ed
Caleb Robinson <caleb.robinson@life.church>
2023-10-03 12:22:44 -0500

    Add seasons table

---
+++ yoyo/migrations/myning_20231002_01_1ui56-add-seasons.sql
@@ -0,0 +1,38 @@
+-- Add seasons
+-- depends: myning_20231001_01_nVple-add-permissions
+
+
+CREATE TABLE "seasons" (
+  "id" SERIAL PRIMARY KEY,
+  "name" varchar,
+  "start_dt" timestamptz,
+  "end_dt" timestamptz,
+  "created_dt" timestamptz,
+  "updated_dt" timestamptz
+);
+
+ALTER TABLE "seasons" ADD CONSTRAINT "seasons_unique_names" UNIQUE ("name");
+
+CREATE TABLE "users_seasons" (
+  "id" SERIAL PRIMARY KEY,
+  "user_id" int,
+  "season_id" int,
+  "created_dt" timestamptz,
+  "updated_dt" timestamptz
+);
+
+
+ALTER TABLE "users_seasons" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+ALTER TABLE "users_seasons" ADD FOREIGN KEY ("season_id") REFERENCES "seasons" ("id");
+
+
+CREATE TABLE "stats" (
+  "id" SERIAL PRIMARY KEY,
+  "user_season_id" int,
+  "key" varchar,
+  "value" float,
+  "created_dt" timestamptz,
+  "updated_dt" timestamptz
+);
+
+ALTER TABLE "stats" ADD FOREIGN KEY ("user_season_id") REFERENCES "users_seasons" ("id");
